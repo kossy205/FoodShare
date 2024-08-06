0a60a4d98a1b568c0b4b7247255ce3d394b5a44b
kossy205 <kosisochukwuhillary45@gmail.com>
2024-08-06 16:03:40 +0100

    recycler view click to see more and claim available food feature completed

--- app/src/main/java/com/kosiso/foodshare/ui/fragments/guest/AvailableFoodDetailsFragment.kt
+++ app/src/main/java/com/kosiso/foodshare/ui/fragments/guest/AvailableFoodDetailsFragment.kt
@@ -23,7 +23,7 @@ class AvailableFoodDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        //// Inflate the layout for this fragment
         binding = FragmentAvailableFoodDetailsBinding.inflate(inflater, container, false)
         return binding.root
     }
